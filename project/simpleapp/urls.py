aee1bbfdc2802c1137751e6d489c533679c46373
Yaroshenko Margarita <_lampochka@mail.ru>
2022-05-31 19:56:02 +0300

    D8 Пример

---
+++ project/simpleapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# Импортируем созданные нами представления
+from django.urls import path
+from .views import Products, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+
+urlpatterns = [
+   path('', Products.as_view()),
+   path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Ссылка на детали товара
+   path('create/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товара
+   path('create/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+   path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
+]
